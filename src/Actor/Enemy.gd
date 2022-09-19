858e331f07cc82c818e2c13d37d2c355f6befada
unknown <athomicus@gmail.com>
2022-09-19 23:15:38 +0200

    Godot files added

--- src/Actor/Enemy.gd
+++ src/Actor/Enemy.gd
@@ -1,14 +1,18 @@
-extends "res://src/Actor/Player.gd"
+extends Actor
+
 
 
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
 
-
+func _physics_process(delta) -> void:
+	var c: = 0
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
